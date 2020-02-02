a715ae3dcaf5b336c754f70983056f67106a77f8
SheepTester <ussa.say@gmail.com>
2020-02-02 12:56:45 -0800

    Properly minify Elm, and don't bundle CSS because it is a pain

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import elm from 'rollup-plugin-elm'
 import serve from 'rollup-plugin-serve'
-import css from 'rollup-plugin-css-only'
 import { terser } from 'rollup-plugin-terser'
 
 const production = process.env.NODE_ENV === 'production'
@@ -16,18 +15,24 @@ export default {
   plugins: [
     elm({
       exclude: 'elm_stuff/**',
-      optimize: production,
-      debug: !production
+      compiler: {
+        optimize: production,
+        debug: !production
+      }
     }),
     process.env.ROLLUP_WATCH && serve({
       contentBase: [''],
       port: 8080
     }),
-    css({
-      output: `${dir}/bundle.css`
-    }),
     production && terser({
-      sourcemap: true
+      sourcemap: true,
+      compress: {
+        pure_funcs: ['F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9'],
+        pure_getters: true,
+        keep_fargs: false,
+        unsafe_comps: true,
+        unsafe: true
+      }
     })
   ],
   watch: {
