46a542efeb1bb8d75e4163ed523bb439c8a56f9c
SheepTester <ussa.say@gmail.com>
2020-02-02 16:41:21 -0800

    Forms and validation

--- src/Base.elm
+++ src/Base.elm
@@ -14,19 +14,47 @@ type HeaderWindow
   | Notifications
   | None
 
+type Input
+  = LoginUsername
+  | LoginPassword
+  | SignUpUsername
+  | SignUpName
+  | SignUpEmail
+  | SignUpPassword
+  | SignUpPasswordAgain
+
 type alias Model =
   { open : HeaderWindow
+  , values :
+    { loginUsername : (String, Bool)
+    , loginPassword : (String, Bool)
+    , signUpUsername : (String, Bool)
+    , signUpName : (String, Bool)
+    , signUpEmail : (String, Bool)
+    , signUpPassword : (String, Bool)
+    , signUpPasswordAgain : (String, Bool)
+    }
   }
 
 init : Api.Session -> Model
 init _ =
   { open = None
+  , values =
+    { loginUsername = ("", False)
+    , loginPassword = ("", False)
+    , signUpUsername = ("", False)
+    , signUpName = ("", False)
+    , signUpEmail = ("", False)
+    , signUpPassword = ("", False)
+    , signUpPasswordAgain = ("", False)
+    }
   }
 
 type Msg
   = Open HeaderWindow
   | Close
   | DontClose
+  | Change Input (String -> String) String
 
 update : Msg -> Model -> (Model, Cmd msg)
 update msg model =
@@ -37,7 +65,31 @@ update msg model =
       ({ model | open = None }, Cmd.none)
     DontClose ->
       (model, Cmd.none)
+    Change input validate value ->
+      let
+        ok = validate value == ""
+        values = model.values
+      in
+        ({ model
+        | values =
+          case input of
+            LoginUsername ->
+              { values | loginUsername = (value, ok) }
+            LoginPassword ->
+              { values | loginPassword = (value, ok) }
+            SignUpUsername ->
+              { values | signUpUsername = (value, ok) }
+            SignUpName ->
+              { values | signUpName = (value, ok) }
+            SignUpEmail ->
+              { values | signUpEmail = (value, ok) }
+            SignUpPassword ->
+              { values | signUpPassword = (value, ok) }
+            SignUpPasswordAgain ->
+              { values | signUpPasswordAgain = (value, ok) }
+        }, Cmd.none)
 
+-- TODO: abstract header windows etc into helper function
 makeHeader : Api.Session -> Model -> List (Html Msg)
 makeHeader session model =
   [ header [ A.class "header" ]
@@ -107,22 +159,26 @@ makeHeader session model =
             ]
             [ text "Log in" ]
           , form [ A.action "./front-page.html", A.class "header-window" ]
-            [ label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Username" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "username", A.placeholder "billygamer5", A.attribute "required" "", A.type_ "text" ]
-                  []
-                ]
-              ]
-            , label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Password" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "password", A.placeholder "hunter2", A.attribute "required" "", A.type_ "password" ]
-                  []
-                ]
-              ]
+            [ Utils.myInput
+              { labelText = "Username"
+              , sublabel = ""
+              , type_ = "text"
+              , placeholder = "billygamer5"
+              , value = Tuple.first model.values.loginUsername
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change LoginUsername
+              }
+            , Utils.myInput
+              { labelText = "Password"
+              , sublabel = ""
+              , type_ = "password"
+              , placeholder = "hunter2"
+              , value = Tuple.first model.values.loginPassword
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change LoginPassword
+              }
             , input [ A.class "button submit-btn", A.type_ "submit", A.value "Log in" ]
               []
             ]
@@ -138,54 +194,60 @@ makeHeader session model =
             ]
             [ text "Sign up" ]
           , form [ A.action "./front-page.html", A.class "header-window" ]
-            [ label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Username" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "username", A.placeholder "billygamer5", A.attribute "required" "", A.type_ "text" ]
-                  []
-                ]
-              , span [ A.class "sublabel" ]
-                [ text "Only letters, digits, underscores, and hyphens are allowed. This cannot be changed later." ]
-              ]
-            , label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Full name" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "name", A.placeholder "Billy Chelontuvier", A.attribute "required" "", A.type_ "text" ]
-                  []
-                ]
-              , span [ A.class "sublabel" ]
-                [ text "Allows others to be able to find you for elimination, which makes the game fair." ]
-              ]
-            , label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Email" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "email", A.placeholder "billygamer5@example.com", A.attribute "required" "", A.type_ "email" ]
-                  []
-                ]
-              , span [ A.class "sublabel" ]
-                [ text "For password reset forms. You can also turn on email notifications if you want." ]
-              ]
-            , label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Password" ]
-              , div [ A.class "input" ]
-                [ input [ A.name "password", A.placeholder "hunter2", A.attribute "required" "", A.type_ "password" ]
-                  []
-                ]
-              , span [ A.class "sublabel" ]
-                [ text "Must be at least 3 poop emoji long." ]
-              ]
-            , label [ A.class "input-wrapper" ]
-              [ span [ A.class "label" ]
-                [ text "Password again" ]
-              , div [ A.class "input" ]
-                [ input [ A.placeholder "hunter2", A.attribute "required" "", A.type_ "password" ]
-                  []
-                ]
-              ]
+            [ Utils.myInput
+              { labelText = "Username"
+              , sublabel = "Only letters, digits, underscores, and hyphens are allowed. This cannot be changed later."
+              , type_ = "text"
+              , placeholder = "billygamer5"
+              , value = Tuple.first model.values.signUpUsername
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change SignUpUsername
+              }
+            , Utils.myInput
+              { labelText = "Full name"
+              , sublabel = "Allows others to be able to find you for elimination, which makes the game fair."
+              , type_ = "text"
+              , placeholder = "Billy Chelontuvier"
+              , value = Tuple.first model.values.signUpName
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change SignUpName
+              }
+            , Utils.myInput
+              { labelText = "Email"
+              , sublabel = "For password reset forms. You can also turn on email notifications if you want."
+              , type_ = "email"
+              , placeholder = "billygamer5@example.com"
+              , value = Tuple.first model.values.signUpEmail
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change SignUpEmail
+              }
+            , Utils.myInput
+              { labelText = "Password"
+              , sublabel = "Must be at least 3 poop emoji long."
+              , type_ = "password"
+              , placeholder = "hunter2"
+              , value = Tuple.first model.values.signUpPassword
+              , validate = \_ -> ""
+              , maxChars = Nothing
+              , storeValueMsg = Change SignUpPassword
+              }
+            , Utils.myInput
+              { labelText = "Password again"
+              , sublabel = ""
+              , type_ = "password"
+              , placeholder = "hunter2"
+              , value = Tuple.first model.values.signUpPasswordAgain
+              , validate = \value ->
+                if value /= Tuple.first model.values.signUpPassword then
+                  "Passwords do not match!"
+                else
+                  ""
+              , maxChars = Nothing
+              , storeValueMsg = Change SignUpPasswordAgain
+              }
             , input [ A.class "button submit-btn", A.type_ "submit", A.value "Sign up" ]
               []
             ]
--- src/Utils.elm
+++ src/Utils.elm
@@ -2,6 +2,7 @@ module Utils exposing (..)
 
 import Html exposing (..)
 import Html.Attributes as A
+import Html.Events as Events
 import Http
 import Json.Encode as E
 import Json.Decode as D
@@ -19,6 +20,43 @@ extLink : String -> String -> String -> Html msg
 extLink linkText url className =
   a [ A.href url, A.rel "noopener noreferrer", A.target "_blank", A.class className ]
     [ text linkText ]
+
+myInput :
+  { labelText : String
+  , sublabel : String
+  , type_ : String
+  , placeholder : String
+  , value : String
+  , validate : String -> String
+  , maxChars : Maybe Int
+  , storeValueMsg : (String -> String) -> String -> msg
+  } -> Html.Html msg
+myInput { labelText, sublabel, type_, placeholder, value, validate, maxChars, storeValueMsg } =
+  let
+    error = validate value
+    ok = error == ""
+  in
+    label [ A.class "input-wrapper", A.classList [ ("error", not ok) ] ]
+      [ span [ A.class "label" ]
+        [ text labelText ]
+      , div [ A.class "input" ]
+        ([ (if type_ == "textarea" then textarea else input)
+          ([ A.placeholder placeholder, A.value value, Events.onInput (storeValueMsg validate) ]
+            ++ if type_ == "textarea" then [] else [ A.type_ type_ ])
+          []
+        ]
+          ++ case maxChars of
+            Just chars ->
+              [ span [ A.class "count" ]
+                [ text ("11/" ++ String.fromInt chars) ]
+              ]
+            Nothing ->
+              [])
+      , span [ A.class "problem" ]
+        [ text error ]
+      , span [ A.class "sublabel" ]
+        [ text sublabel ]
+      ]
 
 host : String
 host = "https://sheep.thingkingland.app/assassin/"
