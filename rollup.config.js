025a8166f37f9f58bcb8066e2ffbc00a2f3289a0
SheepTester <ussa.say@gmail.com>
2020-02-21 23:05:41 -0800

    Steal code from two sources to make the ideal CSS bundler

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,10 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import processEnv from './rollup-plugin-process-env.js'
 import html from 'rollup-plugin-bundle-html'
-import css from 'rollup-plugin-css-porter'
+import CleanCSS from 'clean-css'
 import path from 'path'
+import fse from 'fs-extra'
+import { createFilter } from 'rollup-pluginutils'
 
 const production = process.env.NODE_ENV === 'production'
 const dir = production ? '.' : './dist'
@@ -41,16 +43,48 @@ export default {
       // undesirable
       ignore: `^\\./(?!${production ? '' : 'dist/'}bundle\\.(js|css))`
     }),
-    css({
-      raw: false,
-      minified: `${dir}/bundle.css`,
-      cleanCSSOptions: {
+    // Based on
+    // https://github.com/RJHwang/rollup-plugin-css-porter/blob/master/src/index.js
+    // and
+    // https://github.com/Evercoder/rollup-plugin-css-bundle/blob/master/src/index.js
+    // to preserve override order and be able to minify
+    (() => {
+      const styles = {}
+      const minifier = new CleanCSS({
         format: production ? undefined : 'beautify',
         level: production ? 2 : 0,
         // ??? This works?
         rebaseTo: path.resolve(__dirname, production ? '..' : '../images')
+      })
+      const cssFilter = createFilter('**/*.css')
+      return {
+        name: 'CSS bundlifier',
+        transform (code, id) {
+          if (!cssFilter(id)) return
+          styles[id] = code
+          return ''
+        },
+        generateBundle (opts, bundle) {
+          for (const [file, { modules }] of Object.entries(bundle)) {
+            const moduleNames = Array.isArray(modules) ? modules : Object.keys(modules)
+            const css = Object.entries(styles)
+              .sort(([a], [b]) => moduleNames.indexOf(a) - moduleNames.indexOf(b))
+              .map(([_, css]) => css)
+              .join('\n')
+            const output = minifier.minify(css)
+            if (output.errors.length) throw new Error(output.errors.join('\n'))
+            if (output.warnings.length) console.warn(output.warnings.join('\n'))
+            fse.outputFile(
+              path.join(
+                path.dirname(opts.file),
+                path.basename(file, path.extname(opts.file)) + '.css'
+              ),
+              output.styles
+            )
+          }
+        }
       }
-    }),
+    })(),
     processEnv(),
     process.env.ROLLUP_WATCH && serve({
       contentBase: production ? [''] : ['dist', 'images'],
