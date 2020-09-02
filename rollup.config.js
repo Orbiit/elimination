ac66116217fdbe868017690150b75306be67de7e
SheepTester <ussa.say@gmail.com>
2020-09-02 15:42:43 -0700

    Update dependencies (lodash AGAIN >A<) due to vulnerabilities, and major update terser and adjust rollup config accordingly. Build.

--- rollup.config.js
+++ rollup.config.js
@@ -91,7 +91,6 @@ export default {
       port: 8080
     }),
     production && terser({
-      sourcemap: true,
       compress: {
         pure_funcs: ['F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9'],
         pure_getters: true,
